1b23a8dfac6afac7bd17cd5eaff87f21497a5a2e
xingbofeng <503908971@qq.com>
2017-07-27 19:46:30 +0800

    add my solutions

--- solutions/002.add-two-numbers/add-two-numbers.c
+++ solutions/002.add-two-numbers/add-two-numbers.c
@@ -5,6 +5,13 @@
  *     struct ListNode *next;
  * };
  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
 	struct ListNode *result, *currentNode, *headNode;
 	headNode = NULL;
