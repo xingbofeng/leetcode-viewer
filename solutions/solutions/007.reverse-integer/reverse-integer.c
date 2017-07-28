3f9d23908b72768b8570078281b8bece4dd84293
xingbofeng <503908971@qq.com>
2017-07-28 19:04:12 +0800

    add my solutions

--- solutions/solutions/007.reverse-integer/reverse-integer.c
+++
@@ -1,9 +0,0 @@
-int reverse(int x) {
-  long long reverseNum = 0;
-    while(x != 0) {
-      long long y = x % 10;
-      x = x / 10;
-      reverseNum = 10 * reverseNum + y;
-    }
-    return (reverseNum < INT_MIN || reverseNum > INT_MAX) ? 0 : reverseNum;
-}
