82d35bb734fd7893f02b8f6869abe1c7f28a7e3c
xingbofeng <503908971@qq.com>
2017-07-28 19:08:52 +0800

    :smile:update my solutions

--- release.sh
+++ release.sh
@@ -1,9 +1,7 @@
-cd solutions/
 lc-spider -c config.json
-cd ..
 npm run generate
 rm -f README.md
 cp ./solutions/README.md ./
 git add .
-git commit -m "add my solutions"
+git commit -m ":smile:update my solutions"
 git push origin gh-pages
